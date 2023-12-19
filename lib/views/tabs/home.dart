116f3bc0d6498cd12090591116cebc38dbe769db
yanoojp <120234755+yanoojp@users.noreply.github.com>
2023-12-19 12:51:52 +0900

    update introduction text

--- lib/views/tabs/home.dart
+++ lib/views/tabs/home.dart
@@ -1,92 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:my_portfolio_website/utils/classes/contribution.dart';
-import 'package:my_portfolio_website/utils/functions/fetch_contributions.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Contribution>>(
-      future: fetchContributions(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxHeight: MediaQuery.of(context).size.height,
-              ),
-              child: IntrinsicHeight(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      width: double.infinity,
-                      height: 200,
-                      decoration: const BoxDecoration(
-                          // image: DecorationImage(
-                          //   fit: BoxFit.cover,
-                          //   // image: NetworkImage('https://your-image-url.com/hero.jpg'),
-                          // ),
-                          ),
-                      child: Container(
-                        padding: const EdgeInsets.all(16.0),
-                        color: Colors.black.withOpacity(0.5),
-                        child: const Text(
-                          'Welcome to My Portfolio',
-                          style: TextStyle(
-                            fontSize: 28,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    const CircleAvatar(
-                      radius: 100,
-                      backgroundImage: AssetImage('assets/portfolio_face.jpg'),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Yushi Nogami (野上雄史)',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Your Job Title / Profession',
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Brief introduction about yourself. This can be a paragraph or two describing who you are, what you do, your experiences, skills, and what you are passionate about.',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(
-                        'Hello! I am [Yushi Nogami (野上雄史)], a professional [Your Profession]. Here, you can explore my work, get to know me, and see what I have to offer.',
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                    ),
-                    // ListView.builder(
-                    //   itemCount: snapshot.data?.length,
-                    //   itemBuilder: (context, index) {
-                    //     // var contribution = snapshot.data[index];
-                    //     // Return a widget that displays the contribution data
-                    //   },
-                    // ),
-                  ],
-                ),
-              ),
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // Container(
+          //   width: double.infinity,
+          //   height: 200,
+          //   decoration: const BoxDecoration(
+          //       // image: DecorationImage(
+          //       //   fit: BoxFit.cover,
+          //       //   // image: NetworkImage('https://your-image-url.com/hero.jpg'),
+          //       // ),
+          //       ),
+          //   child: Container(
+          //     padding: const EdgeInsets.all(16.0),
+          //     color: Colors.black.withOpacity(0.5),
+          //     child: const Text(
+          //       'Welcome to My Portfolio',
+          //       style: TextStyle(
+          //         fontSize: 28,
+          //         color: Colors.white,
+          //         fontWeight: FontWeight.bold,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          const SizedBox(height: 16),
+          const CircleAvatar(
+            radius: 100,
+            backgroundImage: AssetImage('assets/face.jpg'),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Yushi Nogami (野上雄史)',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'Your Job Title / Profession',
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "Hello! Welcome to my digital space. \n"
+              "I am Yushi Nogami (野上雄史), a [Your Job Title/Profession], with a passion for [a few key interests related to your profession].\n"
+              "My journey in [Your Field/Industry] began [mention when or how you started, or what inspired you].\n"
+              "Since then, I have [talk about key achievements, experiences, or projects]. \n"
+              "My work revolves around [briefly describe your work and what makes it unique or your approach to it]. \n"
+              "I believe in [mention your work ethic, approach, or philosophy in your profession]. \n"
+              "On this website, you'll find a showcase of my projects, insights into my creative process, and a glimpse of what drives my professional endeavors. \n"
+              "Whether you are a potential client, a fellow [profession], or just someone interested in [relevant field], I hope you find something here that inspires you.\n"
+              "Feel free to connect with me or explore my work. Let's embark on this visual journey together!",
+              style: Theme.of(context).textTheme.bodyLarge,
             ),
-          );
-        }
-      },
+          ),
+          ///////////// connected to local server ///////////
+          ///////////// check my_portfolio_node_js ///////////
+          // Expanded(
+          //   child: Image.network(
+          //     'http://localhost:3000/github-stats',
+          //     fit: BoxFit.cover,
+          //   ),
+          // )
+        ],
+      ),
     );
   }
 }
